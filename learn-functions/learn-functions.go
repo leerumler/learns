b4e177bad2981bbe438a8ed1dfab7da674d65fc9
Lee Rumler <lee@howdolinux.com>
2016-07-05 04:56:35 -0400

    Merge creating functions

--- learn-functions/learn-functions.go
+++ learn-functions/learn-functions.go
@@ -2,11 +2,19 @@ package main
 
 import "fmt"
 
+// from learn-addition.go
+func add(x, y int) int {
+	return x + y
+}
+
+// from learn-swap-strings.go
 func swap(x, y string) (string, string) {
 	return y, x
 }
 
 func main() {
+	fmt.Println(add(42, 133))
+
 	a, b := swap("hello", "world")
 	fmt.Println(b, a)
 }
